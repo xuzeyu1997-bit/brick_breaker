64e435f0f745400ffc09760211a6d605f1c61171
zeyu.xu <xuzeyu1997@gmail.com>
2026-06-23 18:26:19 +0800

    add ball

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math' as math;                                  
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
@@ -15,6 +16,7 @@ class BrickBreaker extends FlameGame {
         ),
       );
 
+  final rand = math.Random();                                  
   double get width => size.x;
   double get height => size.y;
 
@@ -25,5 +27,18 @@ class BrickBreaker extends FlameGame {
     camera.viewfinder.anchor = Anchor.topLeft;
 
     world.add(PlayArea());
+
+    world.add(
+      Ball(                                                     
+        radius: ballRadius,
+        position: size / 2,
+        velocity: Vector2(
+          (rand.nextDouble() - 0.5) * width,
+          height * 0.2,
+        ).normalized()..scale(height / 4),
+      ),
+    );
+
+    debugMode = true;                                           
   }
 }
