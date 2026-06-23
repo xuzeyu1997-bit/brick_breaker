6eabd27991bcedec00c555b3ff2f31f832d57fb7
zeyu.xu <xuzeyu1997@gmail.com>
2026-06-23 17:07:08 +0800

    create game

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:brick_breaker/src/brick_breaker.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final game = FlameGame();
+  final game = BrickBreaker();
   runApp(GameWidget(game: game));
 }
